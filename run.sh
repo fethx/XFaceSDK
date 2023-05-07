feed16111a14efdc60205bb160a351de352eb991
weijia <realweijiaxie@gmail.com>
2023-05-07 04:36:07 -0400

    edit cmake

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 ###
  # @Description: 
  # @Date: 2023-04-24 10:08:08
- # @LastEditTime: 2023-05-06 04:27:05
+ # @LastEditTime: 2023-05-07 04:29:45
  # @FilePath: /XFaceSDK/run.sh
 ### 
 
 cmake -B build
-cmake --build build --target xfaceapp
+cmake --build build --target xfaceapp -- -j8
 build/xfaceapp/xfaceapp
